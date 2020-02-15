a12f7b3d06177f7fa65319005a19f892900f4f57
GuilhermeVBeira <gui_doggao@hotmail.com>
2020-02-15 16:03:55 -0300

    [aula-3] Recuperando dados da requisição

--- aula3/urls.py
+++ aula3/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import index, setacookie, redireciona
+from . import views
 
 app_name = "aula3"
 
 urlpatterns = [
-    path('', index),
-    path('cookie', setacookie),
-    path('uol', redireciona)
+    path('', views.index),
+    path('cookie', views.setacookie),
+    path('uol', views.redireciona),
+    path('<int:code>', views.show_code),
+    path('cat/<int:code>', views.cat_status),
+    path('get/', views.show_get_values),
+    path('post/', views.show_post_values),
+
 ] 
--- aula3/views.py
+++ aula3/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
+
 
 def index(request):
     html = f"<h1>Bem vindo à </h1>"
@@ -17,3 +19,42 @@ def setacookie(request):
 
 def redireciona(request):
     return HttpResponseRedirect('https://uol.com.br')
+
+
+def show_code(request, code):
+    html = f"<h1>O código é {code} </h1>"
+    response = HttpResponse(html)
+    return response
+
+
+def cat_status(request, code):
+    return HttpResponseRedirect(f'https://http.cat/{code}')
+
+
+def show_get_values(request):
+    nome = request.GET.get("nome", None)
+    if nome is None:
+        html = f"<h1>Bem vindo usuário anonimo</h1>"
+    else:
+        html = f"<h1>Bem vindo {nome}</h1>"
+    return HttpResponse(html)
+
+
+@csrf_exempt
+def show_post_values(request):
+    head = ""
+    if request.method == "POST":
+        nome = request.POST.get("nome")
+        sobrenome = request.POST.get("sobrenome")
+        head += f"<h1> bem vindo {nome} {sobrenome}</h1>"
+    html = """
+    <form method=POST>
+    <label for="nome">First name:</label><br>
+    <input type="text" id="nome" name="nome" value=""><br>
+    <label for="sobrenome">Last name:</label><br>
+    <input type="text" id="sobrenome" name="sobrenome" value=""><br><br>
+    <input type="submit" value="Enviar">
+    </form> 
+    """
+    html_to_response = head+html
+    return HttpResponse(html_to_response)
