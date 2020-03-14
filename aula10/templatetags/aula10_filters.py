aa98fe50c364be048a671c31216e8a5be140f7f6
GuilhermeVBeira <gui_doggao@hotmail.com>
2020-03-14 15:49:52 -0300

    [aula-10] Templat tags

--- aula10/templatetags/aula10_filters.py
+++ aula10/templatetags/aula10_filters.py
@@ -1,5 +1,6 @@
 from django import template
 from django.template.defaultfilters import stringfilter
+from aula8.models import Pet
 
 register = template.Library()
 
@@ -8,3 +9,14 @@ register = template.Library()
 @stringfilter
 def swap(value):
     return value.swapcase()
+
+@register.filter
+@stringfilter
+def bold(value):
+    return f"<b>{value}</b>"
+
+
+@register.simple_tag
+def pega_pet_pelo_nome(pet_name):
+    pet = Pet.objects.get(nome=pet_name)
+    return pet
