cf5eeb3e5106c23235ecb5c37a9c8919282a491d
GuilhermeVBeira <gui_doggao@hotmail.com>
2020-04-18 10:59:56 -0300

    [aula-16] throttling

--- aula16/api.py
+++ aula16/api.py
@@ -1,5 +1,7 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.throttling import AnonRateThrottle
 from .models import Hero
 from .serializers import HeroSerializer
 
@@ -8,3 +10,14 @@ class HeroViewSet(ModelViewSet):
     model = Hero
     serializer_class = HeroSerializer
     queryset = Hero.objects.all()
+
+    @action(methods=["get"], detail=False, url_path="poderes")
+    def show_list_hero_powers(self, request):
+        heros = self.get_queryset()
+        results = [{hero.name: hero.super_power} for hero in heros]
+        return Response({"results": results})
+    
+    @action(methods=["get"], detail=True, url_path="poder")
+    def heropower(self, request, pk=None):
+        hero = self.get_object()
+        return Response({"hero": hero.super_power})
--- cursodjango/settings.py
+++ cursodjango/settings.py
@@ -145,4 +145,10 @@ INTERNAL_IPS = [
 SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
 
 REST_FRAMEWORK = {
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "5/hour"
+    }
 }
